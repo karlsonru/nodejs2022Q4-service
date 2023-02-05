61b947a6ddea57d884d79ff63c30eb809a1aaa30
karlsonru <ivpolovnikov@hotmail.com>
2023-02-05 13:17:58 +0300

    feat: running PORT based on env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const PORT = process.env.PORT ?? 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  await app.listen(PORT);
+  console.log(`Server is running on port ${PORT}`);
 }
+
 bootstrap();
